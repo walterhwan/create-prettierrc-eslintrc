3bd3b4f8d23294309f40ffc7053b5a14cca5a596
Walter Wan <walter@trayt.io>
2019-06-21 12:15:19 -0700

    First commit

---
+++ cli.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+const fs = require('fs')
+
+const [,, ...args] = process.argv
+
+const eslintrcFileName = '.eslintrc'
+const eslintrc = {
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "off",
+    "strict": ["error", "global"],
+    "curly": "warn"
+  }
+}
+const eslintrcString = JSON.stringify(eslintrc, null, 2)
+
+const prettierrcFileName = '.prettierrc'
+const prettierrc = {
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "semi": true
+}
+const prettierrcString = JSON.stringify(prettierrc, null, 2)
+
+
+fs.writeFile(eslintrcFileName, eslintrcString, (err) => {
+  if (err) console.log(err)
+  console.log(`File ${eslintrcFileName} created`)
+})
+
+fs.writeFile(prettierrcFileName, prettierrcString, (err) => {
+  if (err) console.log(err)
+  console.log(`File ${prettierrcFileName} created`)
+})
